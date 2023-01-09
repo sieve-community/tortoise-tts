029d4b13fd4ff5809bbb916d92bd5eb446d5e866
Mokshith Voodarla <mvoodarla@gmail.com>
2023-01-09 13:38:25 -0800

    things

--- tortoise/api.py
+++ tortoise/api.py
@@ -181,7 +181,7 @@ def pick_best_batch_size_for_gpu():
     Tries to pick a batch size that will fit in your GPU. These sizes aren't guaranteed to work, but they should give
     you a good shot.
     """
-    return 4
+    return 2
 
 
 class TextToSpeech:
